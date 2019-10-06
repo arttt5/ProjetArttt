5719abd55ee9e7e76eef9c4aecf3d9277b40a64a
ThomasFortrie <thomas.fortrie@hotmail.fr>
2019-10-06 17:31:58 +0200

    Je continue à mettre en place la partie admin

--- partie_admin/admin.php
+++ partie_admin/admin.php
@@ -2,52 +2,52 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../bootstrap.min.css">
-    <link rel="stylesheet" href="admin.css">
-    <title>ADMINISTRATION ROBISHOP</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../bootstrap.min.css">
+  <link rel="stylesheet" href="admin.css">
+  <title>ADMINISTRATION ROBISHOP</title>
 </head>
 
 <body>
-    <div class="container-fluid">
-        <header>
-            <?php include 'header.php'; ?>
-        </header>
+  <div class="container-fluid">
+    <header>
 
-        <div class="connect-admin row mx-auto justify-content-center">
-            <img src="images/Robishop2.png" alt="logo" title="logo" width="60%">
-            <div class="col-md-12 titre">
-                <p><small>Admin Network</small></p>
-                <h4>Veuillez vous connecter</h4>
-            </div>
-            <br>
-            <form action="tableaudebord.php">
-  <div class="form-group">
-    <label for="exampleInputEmail1">LOGIN</label>
-    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Enter Login">
-    <small id="textHelp" class="form-text text-muted">Nous ne communiqueront pas votre email.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  <br><br>
-</form>
+    </header>
 
-
+    <div class="connect-admin row mx-auto justify-content-center">
+      <img src="images/Robishop2.png" alt="logo" title="logo" width="60%">
+      <div class="col-md-12 titre">
+        <p><small>Admin Network</small></p>
+        <h4>Veuillez vous connecter</h4>
+      </div>
+      <br>
+      <form action="tableaudebord.php" method="POST">
+        <div class="form-group">
+          <label for="exampleInputEmail1">LOGIN</label>
+          <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Enter Login">
+          <small id="textHelp" class="form-text text-muted">Nous ne communiqueront pas votre email.</small>
         </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <br><br>
+      </form>
 
-        <footer>
-            <?php include 'footer.php'; ?>
-        </footer>
+
     </div>
+
+    <footer>
+
+    </footer>
+  </div>
 </body>
 
 </html>
---
+++ partie_admin/tableaudebord.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <link rel="stylesheet" href="admin.css">
+    <title>Tableau de bord</title>
+</head>
+
+<body>
+    <div class="container-fluid mx-auto justify-content-center">
+
+
+        <div class="connect-admin row mx-auto justify-content-center">
+            <img src="images/Robishop2.png" alt="logo" title="logo" width="60%">
+            <div class="col-md-12 titre">
+                <p><small>Admin Network</small></p>
+
+            </div>
+
+        </div>
+        <div class="row justify-content-center ">
+            <div class="col-md-1 col-sm-1 "></div>
+            <div class="col-md-5 col-sm-5 option">
+                <a href="produit/gestionproduit.php">GESTION DES PRODUITS</a>
+            </div>
+            <div class="col-md-5 col-sm-5 option">
+                <a href="membre/gestionmembre.php">GESTION DES MEMBRES</a>
+            </div>
+            <div class="col-md-1 col-sm-1"></div>
+        </div>
+
+<div class="row justify-content-center">
+    <a href="informationadmin.php">INFORMATION / RUPTURE / COMMANDE</a>
+</div>
+    </div>
+</body>
+
+</html>
