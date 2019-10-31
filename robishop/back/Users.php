3ceb4a847f96683726d0064b8b730ce2b917987b
Fortrie_Thomas <thomas.fortrie@live.fr>
2019-10-31 14:56:22 +0100

    Création class / attribut / getter/setter

---
+++ robishop/back/Users.php
@@ -0,0 +1,307 @@
+<?php
+
+class Users{
+
+    // Attributs
+
+    private $idUsers;
+    private $nomUsers;
+    private $prenomUsers;
+    private $adresseUsers;
+    private $codePostalUsers;
+    private $villeUsers;
+    private $adresseFacturationUsers;
+    private $codePostalFacturationUsers;
+    private $villeFacturationUsers;
+    private $mailUsers;
+    private $telUsers;
+    private $pseudoUsers;
+    private $motDePasseUsers;
+    private $adminUsers;
+    
+
+
+    /**
+     * Get the value of idUsers
+     */ 
+    public function getIdUsers()
+    {
+        return $this->idUsers;
+    }
+
+    /**
+     * Set the value of idUsers
+     *
+     * @return  self
+     */ 
+    public function setIdUsers($idUsers)
+    {
+        $this->idUsers = $idUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nomUsers
+     */ 
+    public function getNomUsers()
+    {
+        return $this->nomUsers;
+    }
+
+    /**
+     * Set the value of nomUsers
+     *
+     * @return  self
+     */ 
+    public function setNomUsers($nomUsers)
+    {
+        $this->nomUsers = $nomUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenomUsers
+     */ 
+    public function getPrenomUsers()
+    {
+        return $this->prenomUsers;
+    }
+
+    /**
+     * Set the value of prenomUsers
+     *
+     * @return  self
+     */ 
+    public function setPrenomUsers($prenomUsers)
+    {
+        $this->prenomUsers = $prenomUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of adresseUsers
+     */ 
+    public function getAdresseUsers()
+    {
+        return $this->adresseUsers;
+    }
+
+    /**
+     * Set the value of adresseUsers
+     *
+     * @return  self
+     */ 
+    public function setAdresseUsers($adresseUsers)
+    {
+        $this->adresseUsers = $adresseUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of codePostalUsers
+     */ 
+    public function getCodePostalUsers()
+    {
+        return $this->codePostalUsers;
+    }
+
+    /**
+     * Set the value of codePostalUsers
+     *
+     * @return  self
+     */ 
+    public function setCodePostalUsers($codePostalUsers)
+    {
+        $this->codePostalUsers = $codePostalUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of villeUsers
+     */ 
+    public function getVilleUsers()
+    {
+        return $this->villeUsers;
+    }
+
+    /**
+     * Set the value of villeUsers
+     *
+     * @return  self
+     */ 
+    public function setVilleUsers($villeUsers)
+    {
+        $this->villeUsers = $villeUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of adresseFacturationUsers
+     */ 
+    public function getAdresseFacturationUsers()
+    {
+        return $this->adresseFacturationUsers;
+    }
+
+    /**
+     * Set the value of adresseFacturationUsers
+     *
+     * @return  self
+     */ 
+    public function setAdresseFacturationUsers($adresseFacturationUsers)
+    {
+        $this->adresseFacturationUsers = $adresseFacturationUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of codePostalFacturationUsers
+     */ 
+    public function getCodePostalFacturationUsers()
+    {
+        return $this->codePostalFacturationUsers;
+    }
+
+    /**
+     * Set the value of codePostalFacturationUsers
+     *
+     * @return  self
+     */ 
+    public function setCodePostalFacturationUsers($codePostalFacturationUsers)
+    {
+        $this->codePostalFacturationUsers = $codePostalFacturationUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of villeFacturationUsers
+     */ 
+    public function getVilleFacturationUsers()
+    {
+        return $this->villeFacturationUsers;
+    }
+
+    /**
+     * Set the value of villeFacturationUsers
+     *
+     * @return  self
+     */ 
+    public function setVilleFacturationUsers($villeFacturationUsers)
+    {
+        $this->villeFacturationUsers = $villeFacturationUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of mailUsers
+     */ 
+    public function getMailUsers()
+    {
+        return $this->mailUsers;
+    }
+
+    /**
+     * Set the value of mailUsers
+     *
+     * @return  self
+     */ 
+    public function setMailUsers($mailUsers)
+    {
+        $this->mailUsers = $mailUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of telUsers
+     */ 
+    public function getTelUsers()
+    {
+        return $this->telUsers;
+    }
+
+    /**
+     * Set the value of telUsers
+     *
+     * @return  self
+     */ 
+    public function setTelUsers($telUsers)
+    {
+        $this->telUsers = $telUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pseudoUsers
+     */ 
+    public function getPseudoUsers()
+    {
+        return $this->pseudoUsers;
+    }
+
+    /**
+     * Set the value of pseudoUsers
+     *
+     * @return  self
+     */ 
+    public function setPseudoUsers($pseudoUsers)
+    {
+        $this->pseudoUsers = $pseudoUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of motDePasseUsers
+     */ 
+    public function getMotDePasseUsers()
+    {
+        return $this->motDePasseUsers;
+    }
+
+    /**
+     * Set the value of motDePasseUsers
+     *
+     * @return  self
+     */ 
+    public function setMotDePasseUsers($motDePasseUsers)
+    {
+        $this->motDePasseUsers = $motDePasseUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of adminUsers
+     */ 
+    public function getAdminUsers()
+    {
+        return $this->adminUsers;
+    }
+
+    /**
+     * Set the value of adminUsers
+     *
+     * @return  self
+     */ 
+    public function setAdminUsers($adminUsers)
+    {
+        $this->adminUsers = $adminUsers;
+
+        return $this;
+    }
+}
+
+
+
+?>
