08181a96ce187d907d67af364103e3c4355dde81
Fortrie_Thomas <thomas.fortrie@live.fr>
2019-10-31 10:47:32 +0100

    Partie Admin

--- robishop/admin/tableaudebord.php
+++ robishop/admin/tableaudebord.php
@@ -13,30 +13,32 @@
 <body>
     <div class="container-fluid mx-auto justify-content-center">
 
-
         <div class="connect-admin row mx-auto justify-content-center">
-            <img src="images/Robishop2.png" alt="logo" title="logo" width="60%">
-            <div class="col-md-12 titre">
-                <p><small>Admin Network</small></p>
 
+            <div class="row">
+                <img class="bellNotif" src="images/bell.gif" alt="">
+               <p>Vente(s) aujourd'hui ( euros)</p>
             </div>
 
+            
+
+                <img src="images/Robishop2.png" alt="logo" title="logo" width="60%">
+                <div class="col-md-12 titre">
+                    <p><small>Admin Network</small></p>
+                </div>
+            
         </div>
-        <div class="row justify-content-center ">
-            <div class="col-md-1 col-sm-1 "></div>
-            <div class="col-md-5 col-sm-5 option">
-                <a href="produit/gestionproduit.php">GESTION DES PRODUITS</a>
+        <div class="row justify-content-center mx-auto ">
+            <div class="col-md-3"></div>
+            <div class="col-md-2">
+                <a class="btn btn-primary" href="produit/gestionproduit.php">PRODUITS</a>
             </div>
-            <div class="col-md-5 col-sm-5 option">
-                <a href="membre/gestionmembre.php">GESTION DES MEMBRES</a>
-            </div>
-            <div class="col-md-1 col-sm-1"></div>
+            <div class="col-md-2">
+                <a class="btn btn-primary" href="membre/gestionmembre.php">MEMBRES</a></div>
+            <div class="col-md-2">
+                <a class="btn btn-primary" href="informationadmin.php">INFORMATIONS</a></div>
+            <div class="col-md-3"></div>
         </div>
-
-<div class="row justify-content-center">
-    <a href="informationadmin.php">INFORMATION / RUPTURE / COMMANDE</a>
-</div>
-    </div>
 </body>
 
 </html>
