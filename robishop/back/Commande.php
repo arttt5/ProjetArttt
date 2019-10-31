0a1410c6f627c9f2ccb95073a5a5cdb9c718904a
Fortrie_Thomas <thomas.fortrie@live.fr>
2019-10-31 16:27:52 +0100

    Création atribut et getter/setter

---
+++ robishop/back/Commande.php
@@ -0,0 +1,25 @@
+<?php
+
+class Commande{
+
+    // Attribut
+
+    private $idFacture;
+    private $idUsers;
+    private $idProduit;
+
+    private $dateCommande;
+    private $montantCommande;
+    private $typePaiement;
+    private $dateLivraison;
+    private $livre;
+    private $modeLivraison;
+    private $fraisLivraison;
+    
+
+
+}
+
+
+
+?>
