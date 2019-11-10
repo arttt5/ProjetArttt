486847246b1d31779a2326e94b218d8aedf99564
asmaerouel <55698394+asmaerouel@users.noreply.github.com>
2019-11-11 00:42:28 +0100

    Update AbstractDataAccess.php

--- robishop/back/dataAccess/AbstractDataAccess.php
+++ robishop/back/dataAccess/AbstractDataAccess.php
@@ -4,8 +4,25 @@ class AbstractDataAccess{
 
 
     function connexion(){
-        $db = new mysqli('localhost','root','','robishop');
+        $db = new mysqli('localhost','root','','base_projet');
+        return $db;
+
     }
+
+
+
+    protected function executerQuery($myDB, $query)
+    {
+        return mysqli_query($myDB, $query);
+    }
+   
+
+
+    protected function fetchResults($resultSet)
+    {
+        return mysqli_fetch_all($resultSet, MYSQLI_ASSOC);
+    }
+
 }
 
 
